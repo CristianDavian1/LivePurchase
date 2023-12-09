722f19cc390df65fd1dd1b00f438b53c5dbc99b8
el-Mamadisimo <cristiandavian1@gmail.com>
2023-12-09 11:07:21 -0500

    add first

---
+++ Ui/controllers/UserController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using models.DTOs;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult<User> GetUser()
+        {
+            User usuario = new User
+            {
+                UserId = 1,
+                UserName = "Carlitos Romero",
+                Email = "carlos990@gmail.com"
+            };
+            return usuario;
+        }
+    }
+}
